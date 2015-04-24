ceb4c9b6530f09af9d12d3dcff6a84d51698df16
justinashleylawii <justinashleylawii@gmail.com>
2015-04-24 10:15:50 -0400

    Refactored code for readability and maintainability. Users can now exclude file extensions and directories from being copied. Users can now opt to hide all console output. Put all project settings inside a remote_directory object, effectively namespacing settings to avoid conflicts with others.

--- Remote Directory.py
+++ Remote Directory.py
@@ -1,246 +1,364 @@
 import sublime, sublime_plugin
 import os
-import shutil
 import fileinput
-import re
-from subprocess import check_call
+import shutil
 
-class putCommand(sublime_plugin.EventListener):
-	def on_post_save(self, view):
-		settings = sublime.load_settings("Remote Directory.sublime-settings")
-		#If project settings are defined, use them, otherwise, use the package settings, otherwise, default
-		local_path_mask = sublime.active_window().active_view().settings().get("local_path_mask", settings.get("local_path_mask", ""))
-		local_file_path = os.path.split(view.file_name())
-		local_file_index = local_file_path[0].find(local_path_mask)
+class Settings():
 
-		if local_file_index == -1:
-			#sublime.message_dialog("The local_path_mask is invalid for the current file. GET aborted.")
-			print local_path_mask
-			return
+	def __init__(self):
 		
-		local_file_index += len(local_path_mask)
+		#Initialize settings
+		settings = sublime.load_settings("Remote Directory.sublime-settings")
 
+		self.settings = {}
+
+		self.settings['local_mask'] = ""
+		self.settings['remote_mask'] = ""
+		self.settings['history_file'] = ""
+		self.settings['exclude_directories'] = []
+		self.settings['exclude_extensions'] = []
+		self.settings['debug'] = False
+
+		# Get project specific settings (legacy)
+		local_path_mask = sublime.active_window().active_view().settings().get("local_path_mask", settings.get("local_path_mask", ""))
 		remote_path_mask = sublime.active_window().active_view().settings().get("remote_path_mask", settings.get("remote_path_mask", ""))
-		remote_file_path = remote_path_mask + view.file_name()[local_file_index:]
+		history_file_name = sublime.active_window().active_view().settings().get("history_file_name", settings.get("history_file_name", "sublime_gethistory.txt"))
 
-		#sublime.message_dialog(remote_file_path)
+		self.settings['local_mask'] = local_path_mask
+		self.settings['remote_mask'] = remote_path_mask
+		self.settings['history_file'] = history_file_name
 
-		if os.access(remote_file_path, os.F_OK):
-			#last_modified refers to the last time the file was changed
-			last_modified = os.stat(remote_file_path).st_mtime
-			#sublime.message_dialog(repr(last_modified))
+		# Get project specific settings from remote_directory object
+		remote_directory = sublime.active_window().active_view().settings().get("remote_directory", None)
 
-			historypath = local_file_path[0] + "\\" + settings.get("history_file_name", "sublime_gethistory.txt") 
+		if remote_directory:
+			for item in remote_directory:
+				self.settings[item] = remote_directory[item]
 
-			#Check to see if the sublime_gethistory file exists
-			if os.access(historypath, os.F_OK):
-				#File Exists! Time to read!
-				historyfile = open(historypath, 'r')
+		# Remove trailing slashes off local and remote masks
+		if self.settings['local_mask'].endswith('/') or self.settings['local_mask'].endswith('\\'):
+			self.settings['local_mask'] = self.settings['local_mask'][:-1]
+		if self.settings['remote_mask'].endswith('/') or self.settings['remote_mask'].endswith('\\'):
+			self.settings['remote_mask'] = self.settings['remote_mask'][:-1]
 
-				#Each line in history file looks like @filename@lastmodified
-				for line in historyfile:
-					if not line or line.isspace():
-						continue
-					else:
-						tokens = line.split("@")
+	def get(self, key):
+		return self.settings.get(key, "")
 
-						#sublime.message_dialog(tokens[1] + " - " + local_file_path[1])
+class History():
+	history_file_path = ""
 
-						if tokens[1] == local_file_path[1]:
-							#This is the line we're looking for!
-							if float(tokens[2]) == last_modified:
-								#They match! We're good to PUT!
-								#sublime.message_dialog("Match found! We're good to PUT!")
+	def __init__(self, history_file_path, debug):
 
-								shutil.copy2(view.file_name(), remote_file_path)
-								historyfile.close()
-								
-								#Update the last_modified in the file
-								newline = "@" + local_file_path[1] + "@" + repr(os.stat(remote_file_path).st_mtime)
+		self.history_file_path = history_file_path
+		self.debug = debug
 
-								for line in fileinput.input(historypath, inplace=True): 
-									if not line or line.isspace():
-										continue
-									else:
-										tokens = line.split("@")
+		# Check to see if file already exists
+		if not os.access(self.history_file_path, os.F_OK):
+			self.create_history_file()
 
-										if tokens[1] == local_file_path[1]:
-											print line.replace(line, newline)
-										else:
-											print line
-											
-								fileinput.close()
+	def create_history_file(self):
 
-								print "Put operation completed successfully"
-								return
-							else:
-								#They don't match! Fail!
-								sublime.message_dialog("Cannot put because the remote file is newer than the local file. Get remote file first.")
+		if not os.access(self.history_file_path, os.F_OK):
+			self.print_message ("create_history_file: Creating history file at '" + self.history_file_path + "'")
+			history_file = open(self.history_file_path, 'a')
+			history_file.close()
 
-								#Get the last time the local file was modified
-								local_lastModified = os.stat(view.file_name()).st_mtime
+	def get_date_modified(self, file_name):
 
-								#Now we need to perform the put anyways
-								if local_lastModified < os.stat(view.file_name()).st_mtime:
-									shutil.copy2(view.file_name(), remote_file_path)
-									historyfile.close()
-									
-									#Update the last_modified in the file
-									newline = "@" + local_file_path[1] + "@" + repr(os.stat(remote_file_path).st_mtime)
+		history_file = open(self.history_file_path, 'r')
+		file_found = False
 
-									for line in fileinput.input(historypath, inplace=True): 
-										if not line or line.isspace():
-											continue
-										else:
-											tokens = line.split("@")
-
-											if tokens[1] == local_file_path[1]:
-												print line.replace(line, newline)
-											else:
-												print line
-												
-									fileinput.close()
-
-									print "Put operation completed successfully"
-								else:
-									print "Put operation cancelled"
-
-								sublime.active_window().active_view().run_command('revert')
-								return
-				else:
-					sublime.message_dialog("Remote file already exists. Need to get file before it can be put")
-					return
+		for line in history_file:
+			if not line.split():
+				continue
 			else:
-				#File Does Not Exist! Failure!
-				sublime.message_dialog("Cannot put because the remote file is newer than the local file. get first.")
-				return
-				
+				tokens = line.split("@")
+
+				if len(tokens) == 2 and tokens[0] == file_name:
+					file_found = True
+					break
+
+		history_file.close()
+
+		if file_found:
+			self.print_message("get_date_modified: " + file_name + " " + tokens[1])
+			return float(tokens[1])
 		else:
-			#File doesn't exist on the remote server. PUT it
-			shutil.copy2(view.file_name(), remote_file_path)
+			self.print_message("get_date_modified: " + file_name + " -1")
+			return -1
 
-			#Record the new last_modified in sublime_gethistory
-			last_modified = os.stat(remote_file_path).st_mtime
-			historypath = local_file_path[0] + "\\" + settings.get("history_file_name", "sublime_gethistory.txt") 
-			newline = "@" + local_file_path[1] + "@" + repr(last_modified) + "\n"
+	def update_history(self, file_name, last_modified):
+		self.print_message("update_history: Updating...")
+		new_line = file_name + "@" + str(last_modified) + "\n"
+		file_found = False
 
-			historyfile = open(historypath, 'a')
-			historyfile.write(newline)
-			historyfile.close()
+		for line in fileinput.input(self.history_file_path, inplace=1):
+			if not line.split():
+				continue
+			else:
+				tokens = line.split("@")
 
-			print "Put operation completed successfully"
+				if len(tokens) == 2 and tokens[0] == file_name:
+					file_found = True
+					print(new_line)
+				else:
+					print(line)
+
+		if not file_found:
+			history_file = open(self.history_file_path, 'a')
+			history_file.write(new_line)
+			history_file.close()
+
+	def print_message(self, message):
+
+		if self.debug:
+			print(message)
+
+class RemoteDirectory():
+
+	def __init__(self):
+		self.settings = Settings()
+
+	def get_file(self, remote_file):
+
+		self.print_message("get_file: " + remote_file)
+		local_file = self.get_directory_from_path_mask(remote_file, self.settings.get("remote_mask"), self.settings.get("local_mask"))
+
+		if local_file == -1:
+			self.print_message("get_file: Skipping file")
 			return
 
+		local_file_path = os.path.split(local_file)
+		remote_file_path = os.path.split(remote_file)
+
+		if not self.is_file_valid(remote_file):
+			return
+
+		# Make sure the local directory exists - This shouldn't really happen but whatever
+		self.create_directory(local_file_path[0])
+
+		history_file_path = os.path.join(local_file_path[0], self.settings.get("history_file"))
+		history = History(history_file_path, self.settings.get("debug"))
+
+		if not os.path.isdir(remote_file):
+			#We can't get a file if it isn't there
+			if os.access(remote_file, os.F_OK):
+				last_modified = os.stat(remote_file).st_mtime
+			else:
+				self.print_message("get_file: File '" + remote_file_path[1] + "' not found. Aborting.")
+				return
+
+			# Get the file and reload in Sublime
+			shutil.copy2(remote_file, local_file)
+			sublime.active_window().active_view().run_command('revert')
+
+			# Update the history file
+			history.update_history(local_file_path[1], last_modified)
+
+			self.print_message("get_file: GET '" + local_file_path[1] + "' completed successfully")
+		else:
+			# We need to get all the files in the directory and call get_file on those
+			for root, dirs, files in os.walk(remote_file):
+				for f in files:
+
+					remote_file = os.path.join(root, f)
+
+					if remote_file != -1:
+						self.get_file(remote_file)
+						
+
+	def put_file(self, local_file):
+
+		self.print_message("put_file: " + local_file)
+		local_file_path = os.path.split(local_file)
+		remote_file = self.get_directory_from_path_mask(local_file, self.settings.get("local_mask"), self.settings.get("remote_mask"))
+		
+		if remote_file == -1:
+			self.print_message("put_file: File not part of mask. Aborting.")
+			return
+
+		remote_file_path = os.path.split(remote_file)
+
+		if not self.is_file_valid(local_file):
+			return
+
+		# Make sure directory exists
+		self.create_directory(remote_file_path[0])
+
+		history_file_path = os.path.join(local_file_path[0], self.settings.get("history_file"))
+		history = History(history_file_path, self.settings.get("debug"))
+		last_modified = 0
+
+		# If we're not trying to PUT a directory
+		if not os.path.isdir(local_file):
+			if os.access(remote_file, os.F_OK):
+				self.print_message("put_file: " + remote_file + " exists.")
+				last_modified = os.stat(remote_file).st_mtime
+				history_last_modified = history.get_date_modified(local_file_path[1])
+
+				if last_modified > history_last_modified:
+					self.print_message("put_file: last_modified: " + str(last_modified) + " history: " + str(history_last_modified))
+					self.print_message("put_file: Remote file is newer than local. Aborting.")
+
+					# FIXME Checking to make sure that local and remote mask have been initialized
+					if self.settings.get("local_mask") != self.settings.get("remote_mask"):
+						sublime.message_dialog("Aborting PUT: Remote file is newer than local. GET first.")
+
+					return -1
+
+			shutil.copy2(local_file, remote_file)
+
+			# if not last_modified:
+			last_modified = os.stat(remote_file).st_mtime
+			history.update_history(local_file_path[1], last_modified)
+
+			self.print_message("put_file: PUT '" + local_file_path[1] + "' completed successfully")
+
+			return
+		else:
+			# We need to put all the subdirectories and files into the remote
+			for root, dirs, files in os.walk(local_file):
+				for f in files:
+
+					local_file = os.path.join(root, f)
+
+					if local_file != -1:
+						self.put_file(local_file)
+
+	def get_directory_from_path_mask(self, file, mask, new_mask):
+
+		file_path = os.path.split(file)
+		path_index = file_path[0].find(mask)
+
+		if path_index == -1:
+			if file == mask:
+				self.print_message("get_directory_from_path_mask: " + new_mask)
+				return new_mask
+			else:
+				self.print_message("get_directory_from_path_mask: Path does not contain mask")
+				return -1
+
+		path_index += len(mask)
+
+		self.print_message("get_directory_from_path_mask: " + os.path.join(new_mask + file_path[0][path_index:], file_path[1]))
+		return os.path.join(new_mask + file_path[0][path_index:], file_path[1])
+
+	def is_file_valid(self, file_path):
+
+		# Check our exclusions against file
+		exclude_directories = self.settings.get("exclude_directories")
+		exclude_extensions = self.settings.get("exclude_extensions")
+
+		file_path = os.path.split(file_path)
+
+		for dir in exclude_directories:
+			if dir in file_path[0]:
+				self.print_message("is_file_vaild: File '" + file_path[1] + "' is in excluded path")
+				return False
+
+		for ext in exclude_extensions:
+			if file_path[1].endswith(ext):
+				self.print_message("is_file_valid: File '" + file_path[1] + "' contains an excluded extension")
+				return False
+
+		return True
+
+	def create_directory(self, path):
+
+		if not os.path.exists(path):
+			if os.path.isfile(path):
+				os.makedirs(os.path.split(path)[0])
+			else:
+				os.makedirs(path)
+
+	def print_message(self, message):
+
+		if self.settings.get("debug"):
+			print(message)
 
 class getCommand(sublime_plugin.TextCommand):
-	def run_(self, args):
-		self.view.window().run_command("get_file", self.view.file_name())
+	def run(self, edit):
 
-class get_fileCommand(sublime_plugin.WindowCommand):
-	def run_(self, f):
-		settings = sublime.load_settings("Remote Directory.sublime-settings")
-		#Example base directory. Real would be C:\Users\username\Documents\programs\
-		local_path_mask = sublime.active_window().active_view().settings().get("local_path_mask", settings.get("local_path_mask", ""))
-		local_file_path = os.path.split(f)
-		local_file_index = local_file_path[0].find(local_path_mask)
+		rd = RemoteDirectory()
+		local_file = self.view.file_name()
+		remote_file = rd.get_directory_from_path_mask(local_file, rd.settings.get("local_mask"), rd.settings.get("remote_mask"))
+		rd.print_message("get: " + remote_file)
 
-		if local_file_index == -1:
-			sublime.message_dialog("The local_path_mask(" + local_path_mask + ") is invalid for the current file. Get operation aborted.")
-			return
-	
-		local_file_index += len(local_path_mask)
+		if remote_file != -1:
+			rd.get_file(remote_file)
 
-		remote_path_mask = sublime.active_window().active_view().settings().get("remote_path_mask", settings.get("remote_path_mask", ""))
-		remote_file_path = remote_path_mask + f[local_file_index:]
+class put(sublime_plugin.EventListener):
+	def on_post_save(self, view):
 
-		#sublime.message_dialog(remote_file_path)
+		rd = RemoteDirectory()
+		local_file = view.file_name()
+		rd.print_message("put: " + local_file)
+		status = rd.put_file(local_file)
 
-		if os.access(remote_file_path, os.F_OK):
-			historypath = local_file_path[0] + "\\" + settings.get("history_file_name", "sublime_gethistory.txt") 
-
-			if not os.access(historypath, os.F_OK):
-				print "Created history file at \"" + historypath + "\""
-				historyfile = open(historypath, 'a')
-				historyfile.close()
-
-			#Each line in history file looks like @filename@lastmodified
-			newline = "@" + local_file_path[1] + "@" + repr(os.stat(remote_file_path).st_mtime)
-
-			filefound = False
-
-			for line in fileinput.input(historypath, inplace=True): 
-				if not line or line.isspace():
-					continue
-				else:
-					tokens = line.split("@")
-
-					if tokens[1] == local_file_path[1]:
-						filefound = True
-						print line.replace(line, newline)
-					else:
-						print line
-
-			fileinput.close()
-
-			if not filefound:
-				historyfile = open(historypath, 'a')
-				historyfile.write(newline)
-				historyfile.close()
-
-			#Ok, copy the file from the remote directory to the local directory
-			local_path = local_path_mask + f[local_file_index:]
-			shutil.copy2(remote_file_path, local_path)
-
-			sublime.active_window().active_view().run_command('revert')
-			print "Get operation completed successfully"
-		else:
-			#There is no file to GET!
-			sublime.message_dialog("Cannot Get file: " + remote_file_path + ". File doesn't exist.")
+		# Not sure if this is what we want
+		if status == -1:
+			view.run_command("revert")
 			return
 
+class putCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+
+		rd = RemoteDirectory()
+		local_file = self.view.file_name()
+		rd.print_message("put: " + local_file)
+		rd.put_file(local_file)
 
 class get_filesCommand(sublime_plugin.WindowCommand):
 	def run(self, files):
-		#Run this function for the file passed
-		for f in files:
-			self.window.run_command("get_file", f)
+
+		rd = RemoteDirectory()
+		rd.print_message("get_files: [" + ', '.join(files) + "]")
+		
+		for file in files:
+			remote_file = rd.get_directory_from_path_mask(file, rd.settings.get("local_mask"), rd.settings.get("remote_mask"))
+
+			if remote_file != -1:
+				rd.get_file(remote_file)
 
 	def is_visible(self, files):
 		return len(files) > 0
 
 class get_dirsCommand(sublime_plugin.WindowCommand):
 	def run(self, dirs):
-		settings = sublime.load_settings("Remote Directory.sublime-settings")
 
-		local_path_mask = sublime.active_window().active_view().settings().get("local_path_mask", settings.get("local_path_mask", ""))
-		remote_path_mask = sublime.active_window().active_view().settings().get("remote_path_mask", settings.get("remote_path_mask", ""))
+		rd = RemoteDirectory()
+		rd.print_message("get_dirs: [" + ', '.join(dirs) + "]")
 
-		for d in dirs:
-			local_file_path = d
-			local_file_index = local_file_path.find(local_path_mask)
+		for dir in dirs:
+			remote_file = rd.get_directory_from_path_mask(dir, rd.settings.get("local_mask"), rd.settings.get("remote_mask"))
 
-			if local_file_index == -1:
-				#sublime.message_dialog("The local_path_mask(path:: " + local_file_path + " mask:: " + local_path_mask + ") is invalid for the current file. GET aborted.")
-				return
+			if remote_file != -1:
+				rd.get_file(remote_file)
 
-			local_file_index += len(local_path_mask)
-			path = d[local_file_index:]
 
-			for root, dirs, files in os.walk(remote_path_mask + path):
-				for f in files:
-					#Take the root, and substract the remote_path_mask
-					root_file_index = root.find(remote_path_mask)
+	def is_visible(self, dirs):
+		return len(dirs) > 0
 
-					root_file_index += len(remote_path_mask)
-					path = local_path_mask + root[root_file_index:]
+class put_filesCommand(sublime_plugin.WindowCommand):
+	def run(self, files):
 
-					#Check to see if the directory exists locally. Create it if it doesn't
-					if not os.path.isdir(path):
-						os.makedirs(path)
-						print "Created directory \"" + path + "\""
+		rd = RemoteDirectory()
+		rd.print_message("put_files: [" + ', '.join(files) + "]")
 
-					print "Getting file \"" + path + "/" + f + "\""
-					self.window.run_command("get_file", path + "\\" + f)
+		for file in files:
+			rd.put_file(file)
+
+	def is_visible(self, files):
+		return len(files) > 0
+
+class put_dirsCommand(sublime_plugin.WindowCommand):
+	def run(self, dirs):
+
+		rd = RemoteDirectory()
+		rd.print_message("put_dirs: [" + ', '.join(dirs) + "]")
+
+		for dir in dirs:
+			rd.put_file(dir)
 
 	def is_visible(self, dirs):
 		return len(dirs) > 0
